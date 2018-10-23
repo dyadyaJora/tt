378fea9dc4c0655ad48b525561844667a4591709
dyadyaJora <ja_massa97@mail.ru>
2018-10-23 08:45:20 +0300

    ALGORITHM

--- lib/src/main-block/main-block.service.dart
+++ lib/src/main-block/main-block.service.dart
@@ -1,7 +1,65 @@
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:angular/core.dart';
 
 @Injectable()
 class MainBlockService {
+	List<int> calc(List<int> nominals, int sum) {
+		nominals.sort();
+		if (nominals[0] <= 0)
+			return null;
+
+		List<int> D = List.filled(sum + 1, 0);
+		Map<int, List<int>> map = new Map();
+		int res;
+
+		int C(int w, int ind) {
+			int cur_ind = w - nominals[ind];
+
+			if (!map.containsKey(cur_ind))
+				map[cur_ind] = [];
+
+			int flag = map[cur_ind].indexOf(nominals[ind]) == -1 ? 1 : 0;
+			int val = flag * nominals[ind];
+
+			return val;
+		};
+
+		void swap(int w, int i) {
+			int c_val = C(w, i);
+			int cur_ind = w - nominals[i];
+			List<int> nw;
+			HashSet<int> a, b;
+
+			nw = (map.containsKey(cur_ind) ? map[cur_ind] : new List<int>(0)) + [nominals[i]];
+			a = HashSet.from(nw);
+			b = HashSet.from(map.containsKey(w) ? map[w] : new List<int>(0));
+
+			if (b.length > 0 && a.length < b.length) return;
+
+			D[w] = D[cur_ind] + c_val;
+			// if (flag)
+			map[w] = nw;
+		};
+
+		for(int w = 1; w <= sum; w++) {
+			for(int i = 0; i < nominals.length; i++) {
+				if (nominals[i] <= w) {
+					int c_val = C(w, i);
+					if (D[w] <= D[w - nominals[i]] + c_val) {
+						swap(w, i);
+					}
+				}
+			}
+		}
+
+		try {
+			res = map[sum].reduce((a, b) {return a+b;});
+			if (res == sum) 
+				return map[sum];
+		} catch (e) {
+			return null;
+		}
+	}
 }
